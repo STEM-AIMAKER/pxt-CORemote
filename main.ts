bd0ce0148e19213fff598059a8fa7a42403fcfda
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-01-13 11:12:21 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 //% weight=10 color=#1E90FF icon="\uf136"
 namespace CORemote {
+    const PCA9685_ADD = 0x41;
+    const MODE1 = 0x00;
+    const PRESCALE = 0xFE;
+    const LED0_ON_L = 0x06;
+    let initialized = false;
     let serialInited = 0
 
     export enum Shake{
@@ -94,36 +99,42 @@ function i2cread(addr: number, reg: number) {
     return val;
 }
 
-// function setFreq(freq: number): void {
-//     // Constrain the frequency
-//     let prescaleval = 25000000;
-//     prescaleval /= 4096;
-//     prescaleval /= freq;
-//     prescaleval -= 1;
-//     let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-//     let oldmode = i2cread(PCA9685_ADD, MODE1);
-//     let newmode = (oldmode & 0x7F) | 0x10; // sleep
-//     i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
-//     i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
-//     i2cwrite(PCA9685_ADD, MODE1, oldmode);
-//     control.waitMicros(5000);
-//     i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
-// }
+function initPCA9685(): void {
+    i2cwrite(PCA9685_ADD, MODE1, 0x00);
+    setFreq(50);
+    initialized = true;
+}
 
-// function setPwm(channel: number, on: number, off: number): void {
-//     if (channel < 0 || channel > 15)
-//         return;
-//     if (!initialized) {
-//         initPCA9685();
-//     }
-//     let buf = pins.createBuffer(5);
-//     buf[0] = LED0_ON_L + 4 * channel;
-//     buf[1] = on & 0xff;
-//     buf[2] = (on >> 8) & 0xff;
-//     buf[3] = off & 0xff;
-//     buf[4] = (off >> 8) & 0xff;
-//     pins.i2cWriteBuffer(PCA9685_ADD, buf);
-// }
+function setFreq(freq: number): void {
+    // Constrain the frequency
+    let prescaleval = 25000000;
+    prescaleval /= 4096;
+    prescaleval /= freq;
+    prescaleval -= 1;
+    let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+    let oldmode = i2cread(PCA9685_ADD, MODE1);
+    let newmode = (oldmode & 0x7F) | 0x10; // sleep
+    i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+    i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+    i2cwrite(PCA9685_ADD, MODE1, oldmode);
+    control.waitMicros(5000);
+    i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
+}
+
+function setPwm(channel: number, on: number, off: number): void {
+    if (channel < 0 || channel > 15)
+        return;
+    if (!initialized) {
+        initPCA9685();
+    }
+    let buf = pins.createBuffer(5);
+    buf[0] = LED0_ON_L + 4 * channel;
+    buf[1] = on & 0xff;
+    buf[2] = (on >> 8) & 0xff;
+    buf[3] = off & 0xff;
+    buf[4] = (off >> 8) & 0xff;
+    pins.i2cWriteBuffer(PCA9685_ADD, buf);
+}
 
 
 //% blockId=GetButton block="Button|key %num|value %value"
@@ -180,7 +191,22 @@ export function GetButton(num: Button, value: ButtonState): boolean {
     //% weight=96
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function onButtonPressed(btn: Button, handler: () => void) {
+export function onButtonPressed(btn: Button, body: Action) {
+    let Pin = 0;
+    pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
+    pins.setPull(DigitalPin.P14, PinPullMode.PullUp);
+    pins.setPull(DigitalPin.P15, PinPullMode.PullUp);
+    pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
+    if (btn == Button.K1) {
+        Pin = DigitalPin.P13;
+    } else if (btn == Button.K2) {
+        Pin = DigitalPin.P14;
+    } else if (btn == Button.K3) {
+        Pin = DigitalPin.P15;
+    } else if (btn == Button.K4) {
+        Pin = DigitalPin.P16;
+    }
+    pins.onPulsed(Pin, PulseValue.Low, body);
 }
     //% blockId=Rocker block="Rocker|value %value"
     //% weight=96
