22e28e3cb9fdf2a231d43ef190d26b83ce62f792
aimakerhk <59435476+stem-hanshin@users.noreply.github.com>
2021-04-29 10:30:58 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -76,74 +76,6 @@ namespace JoystickController {
         RightDown=9        
     }
 
-function initSerial(): void {
-        if (0 == serialInited) {
-            serial.redirect(
-                SerialPin.P12,
-                SerialPin.P14,
-                BaudRate.BaudRate9600
-            );
-            serialInited = 1;
-        }
-    }
-
-
-function i2cwrite(addr: number, reg: number, value: number) {
-    let buf = pins.createBuffer(2);
-    buf[0] = reg;
-    buf[1] = value;
-    pins.i2cWriteBuffer(addr, buf);
-}
-
-function i2ccmd(addr: number, value: number) {
-    let buf2 = pins.createBuffer(1);
-    buf2[0] = value;
-    pins.i2cWriteBuffer(addr, buf2);
-}
-
-function i2cread(addr: number, reg: number) {
-    pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-    let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-    return val;
-}
-
-function initPCA9685(): void {
-    i2cwrite(PCA9685_ADD, MODE1, 0x00);
-    setFreq(50);
-    initialized = true;
-}
-
-function setFreq(freq: number): void {
-    // Constrain the frequency
-    let prescaleval = 25000000;
-    prescaleval /= 4096;
-    prescaleval /= freq;
-    prescaleval -= 1;
-    let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-    let oldmode = i2cread(PCA9685_ADD, MODE1);
-    let newmode = (oldmode & 0x7F) | 0x10; // sleep
-    i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
-    i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
-    i2cwrite(PCA9685_ADD, MODE1, oldmode);
-    control.waitMicros(5000);
-    i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
-}
-
-function setPwm(channel: number, on: number, off: number): void {
-    if (channel < 0 || channel > 15)
-        return;
-    if (!initialized) {
-        initPCA9685();
-    }
-    let buf = pins.createBuffer(5);
-    buf[0] = LED0_ON_L + 4 * channel;
-    buf[1] = on & 0xff;
-    buf[2] = (on >> 8) & 0xff;
-    buf[3] = off & 0xff;
-    buf[4] = (off >> 8) & 0xff;
-    pins.i2cWriteBuffer(PCA9685_ADD, buf);
-}
-
 
 //% blockId=GetButton block="Button|key %num|value %value"
 //% weight=96
@@ -345,11 +277,11 @@ export function onButtonPressed(btn: Button, body: Action) {
     export function Remote_Shake(value: Shake): void {
         switch (value) {
             case Shake.ON: {
-                setPwm(0, 0, 4095);
+                pins.digitalWritePin(DigitalPin.P12, 1)
                 break;
             }
             case Shake.OFF: {
-                setPwm(0, 0, 0);
+                pins.digitalWritePin(DigitalPin.P12, 0)
                 break;
             }
         }
